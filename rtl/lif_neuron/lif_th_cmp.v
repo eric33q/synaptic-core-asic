166e47929c37fb4b0c4947438d39b6c4fa0b616c
eric33q <eric2050130@gmail.com>
2025-11-16 22:40:35 +0800

    lif_th_cmp語法修改

--- rtl/lif_neuron/lif_th_cmp.v
+++ rtl/lif_neuron/lif_th_cmp.v
@@ -1,11 +1,13 @@
-module lif_th_cmp(V_mem, spike);
+module lif_th_cmp#(
+    parameter DATA_WIDTH = 8
+)(V_mem, spike);
     
-    input [7:0] V_mem
-    output reg spike
+    input [DATA_WIDTH-1:0] V_mem;
+    output reg spike;
     
-    parameter [7:0] V_th = 8'd20;
+    localparam [DATA_WIDTH-1:0] V_th = 8'd20;
     
-    always @(*) begin
-        spike = (V_mem >= V_th);
+    always @(*)begin
+        spike = (V_mem >= V_th) ? 1'b1 : 1'b0;
     end
 endmodule
