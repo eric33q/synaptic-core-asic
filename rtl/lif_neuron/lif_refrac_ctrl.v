f76b7aadc282f2d0e2b952758a277c935cf27bc4
Mina Lin <mina2005520@gmail.com>
2025-12-11 23:14:17 +0800

    讓計數器 cnt 的位寬能自動適應 REF_PERIOD 的大小

--- rtl/lif_neuron/lif_refrac_ctrl.v
+++ rtl/lif_neuron/lif_refrac_ctrl.v
@@ -7,20 +7,22 @@ module lif_refrac_ctrl #(
     output wire ref_active
 );
     // 計算不應期
-    reg [3:0] cnt;
+    // 自動計算計數器所需的位寬，以容納 REF_PERIOD
+    localparam CNT_WIDTH = (REF_PERIOD == 0) ? 1 : $clog2(REF_PERIOD + 1);
+    reg [CNT_WIDTH-1:0] cnt;
     //spike後才開始數
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            cnt <= 4'd0;
+            cnt <= 0;
         end else begin
             if (post_spike) begin
-                cnt <= REF_PERIOD[3:0];
+                cnt <= REF_PERIOD;
             end else if (cnt > 0) begin
                 cnt <= cnt - 1'b1;
             end
         end
     end
     // 不為0即在不應期ref_active=1
-    assign ref_active = (cnt != 4'd0);
+    assign ref_active = (cnt != 0);
 
 endmodule
