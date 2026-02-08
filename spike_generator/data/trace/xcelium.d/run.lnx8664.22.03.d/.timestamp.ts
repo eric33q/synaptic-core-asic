106b76801e6a7ccf86da09e76ff2404cb565e9aa
lan <t112830043@ntut.org.tw>
2026-02-08 19:38:04 +0800

    remove file from repo

--- spike_generator/data/trace/xcelium.d/run.lnx8664.22.03.d/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1770392432 /home/t112830043/synaptic-core-asic/rtl/trace/pre_trace.v
-1770382483 /home/t112830043/synaptic-core-asic/rtl/trace/trace_core.v
-1770393466 /home/t112830043/synaptic-core-asic/rtl/trace/pre_trace_tb.v
