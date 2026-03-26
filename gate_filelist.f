625cdff3a316efbea91c154db18e1f1e80812594
MinaLin20 <mina2005520@gmail.com>
2026-03-27 04:17:11 +0800

    add gate_filelist

---
+++ gate_filelist.f
@@ -0,0 +1,8 @@
+// TSMC 90nm 標準元件庫 (Cell Library Simulation Model)
+/home/cell_lib/CBDK_TSMC90GUTM_Arm_f1.0/CIC/Verilog/tsmc090.v
+// 合成後的 Netlist 檔案
+../../syn/netlist/top_syn.v
+// SRAM 模擬模型 (因為合成時 SRAM 只是黑盒子，模擬時還是需要提供行為模型)
+../../rtl/top/weight_memory/sram_sp_128x64.v
+../../rtl/top/weight_memory/spike_gen_mem.v
+../../rtl/top/weight_memory/pre_trace_mem.v
